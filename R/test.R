2935d94ea05d4ba017eeb32324327eb61118826e
emolinab <mbacca@pik-potsdam.de>
2022-07-11 11:38:34 +0200

    basic files mrISIMIP repository

---
+++ R/test.R
@@ -0,0 +1,13 @@
+#' Fahrenheit conversion
+#'
+#' Convert degrees Fahrenheit temperatures to degrees Celsius
+#' @param F_temp The temperature in degrees Fahrenheit
+#' @return The temperature in degrees Celsius
+#' @examples
+#' temp1 <- F_to_C(50);
+#' temp2 <- F_to_C( c(50, 63, 23) );
+#' @export
+test<-function(F_temp){
+  C_temp <- (F_temp - 32) * 5/9;
+  return(C_temp);
+}
